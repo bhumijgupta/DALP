e4c9774c9410b1077ec4ba75d90df55107656f19
allandhir <48802272+allandhir@users.noreply.github.com>
2020-03-31 00:34:36 +0530

    speech functions added TeacherDashboard.js

--- website/src/components/TeacherDashboard.js
+++ website/src/components/TeacherDashboard.js
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 import Peer from "peerjs";
 import io from "socket.io-client";
 import { Redirect } from "react-router-dom";
+import * as SpeechSDK from "microsoft-cognitiveservices-speech-sdk";
 
 import NavBar from "./NavBar";
 import "./TeacherDashboard.css";
 import Video from "./Video";
+import SpeechTextArea from './SpeechTextArea'
 export class TeacherDashboard extends Component {
   state = {
     img: "",
@@ -15,7 +17,10 @@ export class TeacherDashboard extends Component {
     joineeList: [],
     conn: null,
     socket: null,
-    socketSet: false
+    socketSet: false,
+    phraseDiv: '',
+    statusDiv: '',
+    reco: ''
   };
 
   componentDidMount = () => {
@@ -67,9 +72,106 @@ export class TeacherDashboard extends Component {
     // Ask recievers to class teacher
     this.state.socket.emit("s-call");
     // Start sending screenshots
-    setInterval(this.sendScreenshot, 3000);
+    // setInterval(this.sendScreenshot, 3000);
     // TODO: Allandhir implement transcript sending
+    var lastRecognized = "";
+    var phraseDivx = "";
+    var statusDivx = "";
+
+    var audioConfig;
+
+    // audioConfig = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
+
+    var pushStream = SpeechSDK.AudioInputStream.createPushStream();
+ navigator.mediaDevices.getUserMedia({ video: true, audio: true}).
+    then((stream) => {
+      
+      pushStream.write(stream);
+      console.log(stream)
+      console.log("STATE")
+      console.log(this.state.stream)
+  
+        // audioConfig = SpeechSDK.AudioConfig.fromStreamInput(pushStream);
+    });
+    
+        // pushStream.write(this.state.stream);
+        // audioConfig = SpeechSDK.AudioConfig.fromStreamInput(pushStream);
+
+    var speechConfig;
+    speechConfig = SpeechSDK.SpeechConfig.fromSubscription(
+      "9711663ef014426fbf2140be66f96488",
+      "centralindia"
+    );
+
+    speechConfig.speechRecognitionLanguage = "en-US";
+
+    var reco;
+    reco = new SpeechSDK.SpeechRecognizer(speechConfig, audioConfig);
+
+    reco.recognizing = (s, e) => {
+      window.console.log("RECO.RECOGNIZING");
+
+      window.console.log(e.result.text);
+
+      phraseDivx = lastRecognized +  e.result.text;
+      this.setState({
+        phraseDiv: phraseDivx,
+        statusDiv: statusDivx
+
+      });
+    };
+    reco.recognized = (s, e) => {
+      window.console.log("RECO.RECOGNIZED");
+
+      window.console.log(e.result.text);
+
+      // Indicates that recognizable speech was not detected, and that recognition is done.
+      if (e.result.reason === SpeechSDK.ResultReason.NoMatch) {
+        var noMatchDetail = SpeechSDK.NoMatchDetails.fromResult(e.result);
+
+        statusDivx += SpeechSDK.ResultReason[e.result.reason] + SpeechSDK.NoMatchReason[noMatchDetail.reason] ;
+
+        //window.console.log(statusDivx);
+      } else {
+        statusDivx += "(recognized)  Reason: " + SpeechSDK.ResultReason[e.result.reason] + " Text: " + e.result.text + "\r\n";
+        //window.console.log(statusDivx);
+      }
+      
+      lastRecognized += e.result.text + "\r\n";
+      phraseDivx = lastRecognized;
+      window.console.log("PHRASEDIVX");
+      window.console.log(phraseDivx);
+
+       this.setState(prevState => ({
+        phraseDiv: phraseDivx,
+      //   statusDiv: statusDivx,
+         reco: reco
+       }));
+      
+    };
+
+    // Starts recognition
+    reco.startContinuousRecognitionAsync();
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   };
+  //STOP RECORD. this function should be executes when STOP STREAMING button is pressed
+  //Donot forget to press "stop" while testing. It costs free hours available
+  stopRecord = () => {
+
+    // window.console.log(this.state.reco)
+    
+    this.state.reco.stopContinuousRecognitionAsync(
+        function () {
+            this.state.reco.close();
+            this.state.reco = undefined;
+        },
+        function (err) {
+            this.state.reco.close();
+            this.state.reco = undefined;
+        })
+        
+  }
+
 
   // TODO: implement present students
   getAllNames = () => {
@@ -130,7 +232,10 @@ export class TeacherDashboard extends Component {
     }
     return (
       <div className="teacher-dashboard mb-3">
+        <button onClick={() => this.stopRecord()}>STOP</button>
         <NavBar name={this.props.TeacherState.name} />
+        
+        <SpeechTextArea phrase={this.state.phraseDiv} /> {/* this.state. phraseDiv has recognized text. */}
         <div className="container content">
           <div className="row">
             <div className="col-md">
