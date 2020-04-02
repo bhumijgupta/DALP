4546861abd2602952439ffd5beadd19d49bdc950
YashMeh <mehrotra.yash9@gmail.com>
2020-04-02 20:18:25 +0530

    Delete course after teacher left

--- backend/utils/socket-functions.js
+++ backend/utils/socket-functions.js
@@ -1,8 +1,8 @@
 const users = [];
 
 // Join user to chat
-function userJoin(id, username, room) {
-  const user = { id, username, room };
+function userJoin(id, username, room, teacher) {
+  const user = { id, username, room, teacher };
 
   users.push(user);
 
