a104618bca53bcacb2cc08c258513add8364b86a
bhumijgupta <bhumijgupta@gmail.com>
2020-09-02 20:28:34 +0530

    📦 NEW: PWA - Add basic PWA boilerplate
    Signed-off-by: bhumijgupta <bhumijgupta@gmail.com>

---
+++ website/public/serviceworker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "version-1-beta";
+const urlsToCache = ["index.html", "offline.html", "bootstrap.min.css"];
+
+const self = this;
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        console.log("Opened cache");
+        return cache.addAll(urlsToCache);
+      })
+      .catch((err) => console.log(err))
+  );
+});
+
+// Listen
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
